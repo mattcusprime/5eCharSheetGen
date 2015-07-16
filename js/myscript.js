2a2dab4c7b91ecbf81692899239ecc80e1629759
mattcusprime <englmt1@gmail.com>
2015-07-16 16:11:22 -0400

    Tweaked where lowest roll of 4 is discarded

--- js/myscript.js
+++ js/myscript.js
@@ -18,9 +18,9 @@ var coreDataSet = [
     { "stat": "STR", "score": 10, "modifier": "<input id='STRMod' type='text' value='0'></input>" },
     { "stat": "DEX", "score": 10, "modifier": "<input id='DEXMod' type='text' value='0'></input>" },
     { "stat": "CON", "score": 10, "modifier": "<input id='CONMod' type='text' value='0'></input>" },
-    { "stat": "INT", "score": 12, "modifier": "<input id='INTMod' type='text' value='0'></input>" },
-    { "stat": "WIS", "score": 12, "modifier": "<input id='WISMod' type='text' value='0'></input>" },
-    { "stat": "CHA", "score": 12, "modifier": "<input id='CHAMod' type='text' value='0'></input>" }
+    { "stat": "INT", "score": 10, "modifier": "<input id='INTMod' type='text' value='0'></input>" },
+    { "stat": "WIS", "score": 10, "modifier": "<input id='WISMod' type='text' value='0'></input>" },
+    { "stat": "CHA", "score": 10, "modifier": "<input id='CHAMod' type='text' value='0'></input>" }
 ]
 var coreTable;
 $('document').ready(function () {
@@ -38,12 +38,21 @@ $('document').ready(function () {
             var thisMod = $(idSelector).val();
             thisMod = Number(thisMod)
             if (typeof (thisMod) == 'undefined'||thisMod == "") { thisMod == 0 };
-            var numNewScore = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + thisMod;
+            var arrNumNewScore = [Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1)];
+            var numMinScore = Math.min(arrNumNewScore[0], arrNumNewScore[1], arrNumNewScore[2], arrNumNewScore[3]);
+            var numNewScore = 0;
+            for (var j = 0; j < arrNumNewScore.length; j++) {
+                numNewScore = numNewScore + arrNumNewScore[j];
+            };
+            numNewScore = numNewScore - numMinScore;
             coreDataSet[i].score = numNewScore;
             coreDataSet[i].modifier = "<input id='" + idSelector.replace('#','') + "' type='text' value='" + thisMod + "'></input>";
         }
         console.log(coreDataSet)
         coreDataTable.clear().rows.add(coreDataSet).draw();
     });
+
+
+
 });
 
